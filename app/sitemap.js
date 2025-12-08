16ebe976fa8a6f9098e27c9276752c2d38fcf42c
Rushikesh <hrishi210402@gmail.com>
2025-12-08 07:53:37 +0530

    sitemaps and robots added

---
+++ app/sitemap.js
@@ -0,0 +1,34 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://sumedh-portfolio-seven.vercel.app/",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1.0,
+    },
+    {
+      url: "https://sumedh-portfolio-seven.vercel.app/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://sumedh-portfolio-seven.vercel.app/services",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://sumedh-portfolio-seven.vercel.app/testimonials",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: "https://sumedh-portfolio-seven.vercel.app/contact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    }
+  ];
+}
